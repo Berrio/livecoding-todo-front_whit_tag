e8d30a36091d4a4ca89e73434c484283330d51ab
JonatanBerrioRoman <berrioromanjonatan@gmail.com>
2022-05-14 14:13:20 -0500

    added tagActions

--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -1,14 +1,15 @@
 import React, { useContext } from 'react'
 import { deleteNote, putNote } from '../../actions/noteActions/noteActions';
 import { Store } from '../../state/StoreProvider';
+import TagList from '../tag/TagList';
 
-const Note = ({note}) => {
+const Note = ({ note }) => {
 
-  const {dispatch} = useContext(Store)
+  const { dispatch } = useContext(Store)
 
-  const onCheckbox = async (e)=> {
+  const onCheckbox = async (e) => {
     const checked = e.currentTarget.checked;
-    const noteWithCheckbox = {...note, done: checked}
+    const noteWithCheckbox = { ...note, done: checked }
     const response = await putNote(noteWithCheckbox)
     const action = {
       type: 'update-note',
@@ -19,7 +20,7 @@ const Note = ({note}) => {
 
   const onDeleteNote = async (id) => {
     const response = await deleteNote(id);
-    if(response.status === 200){
+    if (response.status === 200) {
       const action = {
         type: 'delete-note',
         payload: note
@@ -28,7 +29,7 @@ const Note = ({note}) => {
     }
   }
 
-  const editNote = ()=>{
+  const editNote = () => {
     const action = {
       type: 'add-note-to-be-updated',
       payload: note
@@ -38,11 +39,24 @@ const Note = ({note}) => {
   return (
     <div>
 
-      <div className="card ">
-        <h3 style={note.done ? { 'textDecoration': 'line-through' } : {}}>{note.message}</h3>
-        <input onChange={onCheckbox} type="checkbox" checked={note.done} />
-        <button className=" btn btn-warning mt-2" onClick={() => onDeleteNote(note.id)}>delete note</button>
-        <button className=" btn btn-secondary mt-1" onClick={editNote}>edit note</button>
+      <div className="card  ">
+        <div className='col-6'>
+
+          <h3 style={note.done ? { 'textDecoration': 'line-through' } : {}}>{note.message}</h3>
+          <input onChange={onCheckbox} type="checkbox" checked={note.done} />
+        </div>
+        <div className='col-4'>
+          <input className="col-6" type="text" name="Tag" placeholder="Tag" />
+          <button className="col-6 btn btn-secondary mt-1" onClick={editNote}>Add Tag</button>
+          <div className='col-4'><TagList note={note.tags  }></TagList></div>
+        </div>
+
+          <button className=" btn btn-warning mt-2" onClick={() => onDeleteNote(note.id)}>delete note</button>
+          <button className=" btn btn-secondary mt-1" onClick={editNote}>edit note</button>
+          {console.log("note Note")}
+          {console.log(note)}
+          
+
       </div>
     </div>
   )
